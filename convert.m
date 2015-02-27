f522db6fae5b5718f4785d9879959b31721a87f7
Cassio M. M. Pereira <cassiomartini@gmail.com>
2015-02-27 20:00:36 -0300

    matlab conversion code and constants tweaking

---
+++ convert.m
@@ -0,0 +1,11 @@
+T = readtable('/home/cassio/work/workspace/subspacestream/subspace_stream.csv');
+X = cell2mat(table2cell(T(:,1:4)));
+rawsup = table2cell(T(:,5));
+uclass = unique(rawsup);
+sup = zeros(size(T, 1),1);
+
+for i = 1:size(uclass,1)
+    sup(strcmp(rawsup, uclass(i))) = i;
+end
+
+save 'subspace_stream.mat' X sup
