7dd1afa4883f40d7aab37229a58cc2dae096d85e
Cassio M. M. Pereira <cassiomartini@gmail.com>
2015-03-24 16:49:06 -0300

    patch for convert to work with any dimensionality

--- convert.m
+++ convert.m
@@ -1,6 +1,7 @@
 T = readtable('C:/Users/Cássio/Dropbox/workspace/subspacestream/subspace_stream.csv');
-X = cell2mat(table2cell(T(:,1:4)));
-rawsup = table2cell(T(:,5));
+ndim = size(T,2);
+X = cell2mat(table2cell(T(:,1:(ndim-1))));
+rawsup = table2cell(T(:,ndim));
 uclass = unique(rawsup);
 sup = zeros(size(T, 1),1);
 
