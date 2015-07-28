d925f17805d471f2f3975632e07d20e0cebf139d
Cassio M. M. Pereira <cassiomartini@gmail.com>
2015-07-28 15:16:15 -0300

    adding 7zip to ignore

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ CP="target/classes;$M2/org/pentaho/pentaho-commons/pentaho-package-manager/1.0.8
 # lambda = 0.0002 -> at w = 5000 prob of model 1 is 0.5
 # 2^(-0.0002 * 5000) = 0.5
 
-NDIM=20
+NDIM=8
 java -Xmx2G -cp $CP br.fapesp.subspacestream.SubspaceStreamGenerator -a 4 -d $NDIM -v -N 100000 -l 0.0001
-java -cp $CP  weka.core.converters.CSVSaver -i subspace_stream.arff -o subspace_stream.csv
-matlab  -noFigureWindows -automation -wait -nosplash -nojvm -nodesktop -r "run('C:/Users/Cássio/Dropbox/workspace/subspacestream/convert.m'); exit;"
+#java -cp $CP  weka.core.converters.CSVSaver -i subspace_stream.arff -o subspace_stream.csv
+#matlab  -noFigureWindows -automation -wait -nosplash -nojvm -nodesktop -r "run('C:/Users/Cássio/Dropbox/workspace/subspacestream/convert.m'); exit;"
 
